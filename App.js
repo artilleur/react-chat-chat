93eb66c08f6d3317ba63a7e5ec35da44bf1273c7
artilleur <99259153+artilleur@users.noreply.github.com>
2024-01-02 14:51:30 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CategoriesList from './CategoriesList';
+import SousCategoriesList from './SousCategoriesList';
+import ProductsList from './ProductsList';
+import ProductDetailList from './ProductDetailList';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Categories">
+        <Stack.Screen name="Categories" component={CategoriesList} />
+        <Stack.Screen name="SousCategories" component={SousCategoriesList} />
+        <Stack.Screen name="Products" component={ProductsList} />
+        <Stack.Screen name="ProductDetail" component={ProductDetailList} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
